8cafd5cf1f0a1657f46238b121c6b7f6866a2e4d
abdelilahtheboss <abdelilah.elhaouchi@gmail.com>
2024-09-11 23:13:00 +0800

    add python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+substraction = number1 - number2
+multiplication = number2 * number1
+
+print('Addition of', number1, 'and', number2, 'is', addition)
+print('Substraction of', number1, 'and', number2, 'is', substraction)
+print('Multiplication of', number2, 'and', number1, 'is', multiplication)
