41b3c174b6ac1251f841ab07bbb397c090d5668a
abdelilahtheboss <abdelilah.elhaouchi@gmail.com>
2024-09-23 07:50:17 +0100

    update changes in daily_reminder.py file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
 task = input("Enter your task: ")
-priority = input("priority (high/medium/low)")
-time_bound = input("Is it time_bound? (yes/no): ")
+priority = input("priority (high/medium/low) ").lower()
+time_bound = input("Is it time_bound? (yes/no): ").lower()
 
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"reminder: '{task}' is a {priority} priority task kthat requires immediate attention today!")
+            print(f"reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"note: '{task}' is a {priority} priority taskk. consider  completing it when you have free time.")
+            print(f"note: '{task}' is a {priority} priority task. consider  completing it when you have free time.")
     case "medium": 
         if time_bound == "yes":
-            print(f"reminder: '{task}' is a {priority} priority task kthat requires immediate attention today!")
+            print(f"reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"note: '{task}' is a {priority} priority taskk. consider  completing it when you have free time.")
+            print(f"note: '{task}' is a {priority} priority task. consider  completing it when you have free time.")
     case "low":
         if time_bound == "yes":
-            print(f"reminder: '{task}' is a {priority} priority task kthat requires immediate attention today!")
+            print(f"reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
-            print(f"reminder: '{task}' is a {priority} priority task kthat requires immediate attention today!")
+            print(f"reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
