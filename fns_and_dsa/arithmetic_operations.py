ec376d54483c0d9e732dbfebda7c6b6142ff593b
abdelilahtheboss <abdelilah.elhaouchi@gmail.com>
2024-09-29 11:29:13 +0100

    update arithmetic_operation.py file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,16 @@
 def perform_operation(num1, num2, operation):
-    if operation=='add': 
+    if operation =='add': 
         result = num1 + num2
         return float(result)
-    elif operation== 'subtract':
+    elif operation == 'subtract':
         result = num1 - num2
         return float(result)
-    elif operation== 'multiply':
+    elif operation == 'multiply':
         result = num1 * num2
         return float(result)
-    elif operation== 'divide':
-        if num2== 0:
-            return 'Sorry, we cannot make this operation.'
+    elif operation == 'divide':
+        if num2 == 0:
+            print('Sorry, we cannot make this operation.')
         else:
             result = num1 / num2
             return float(result)
