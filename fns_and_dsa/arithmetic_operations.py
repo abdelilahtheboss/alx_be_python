6cd886b1351cf2ab87bb24441077a11b5327985f
abdelilahtheboss <abdelilah.elhaouchi@gmail.com>
2024-09-29 11:22:13 +0100

    adding the fns_and_dsa file

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+    if operation=='add': 
+        result = num1 + num2
+        return float(result)
+    elif operation== 'subtract':
+        result = num1 - num2
+        return float(result)
+    elif operation== 'multiply':
+        result = num1 * num2
+        return float(result)
+    elif operation== 'divide':
+        if num2== 0:
+            return 'Sorry, we cannot make this operation.'
+        else:
+            result = num1 / num2
+            return float(result)
+
+
+    
